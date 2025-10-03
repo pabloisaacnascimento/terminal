828ec875e0c101f2e0557cd8484f7d95e64b48e7
pabloisaacnascimento <pabloisaacnascimento@gmail.com>
2025-10-03 01:07:07 -0300

    configurado o onclick no botão

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const addText = document.getElementById('text');
+addText.innerHTML = "Olá, eu fui adicionado via JavaScript!";
+
+function cliquei(){
+
+    alert(addText.innerHTML = "Você clicou no botão!");
+}
